b6fb2d068c2320606e559b95a68a29b481d27cc3
cmcdermitt <charliemc1996@gmail.com>
2018-11-29 15:45:22 -0500

    updated

--- App.js
+++ App.js
@@ -1,58 +1,17 @@
 import React from 'react';
-import { Text, View, WebView, StyleSheet, SectionList } from 'react-native';
+import { Text, View, WebView, StyleSheet, FlatList, Image, Dimensions } from 'react-native';
 import { Location, Permissions, Calendar } from 'expo';
-import { Button, Drawer, ListItem, ActionButton } from 'react-native-material-ui';
+import { Button, Drawer, ListItem, ActionButton, ThemeContext, getTheme, COLOR } from 'react-native-material-ui';
+
+//set theme for react-native-material-ui elements
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.indigo500,
+    accentColor: COLOR.pinkA400,
+  }
+};
 
 //the code stuff
-
-var currentLocation = getLocationAsync().then(locationSuccessCallback, locationFailureCallback);
-var events = getEventsAsync().then(eventSuccessCallback, eventFailureCallback);
-
-
-function locationSuccessCallback(result) {
-  currentLocation = [result.coords.latitude, result.coords.longitude]
-  console.log("The location is " + currentLocation);
-
-}
-
-function locationFailureCallback(error) {
-  console.log("you done broke location: " + error);
-}
-
-function eventSuccessCallback(result) {
-  events=result;
-  // console.log("The events are " + JSON.stringify(events));
-}
-
-function eventFailureCallback(error) {
-  console.log("you done broke the events: " + error);
-}
-
-async function getLocationAsync() {
-  const { status } = await Permissions.askAsync(Permissions.LOCATION);
-  if (status === 'granted') {
-    return Location.getCurrentPositionAsync({enableHighAccuracy: true});
-  } else {
-    throw new Error('Location permission not granted');
-  }
-}
-
-async function getEventsAsync() {
-  const { status } = await Permissions.askAsync(Permissions.CALENDAR);
-  if (status === 'granted') {
-    var calendars;
-    calendars = await(Calendar.getCalendarsAsync());
-    var ids = [];
-    for (cal in calendars) {
-      ids.push(calendars[cal].id)
-    }
-    //from one hour ago to one day in the future
-    time = Date.now();
-    return Calendar.getEventsAsync(ids, time - 3.6e6, time + 8.64e+7);
-  } else {
-    throw new Error('Location permission not granted');
-  }
-}
 
 //the layout stuff
 
@@ -70,50 +29,96 @@ function eventListObj(props) {
   });
 }
 
-class Schedule extends React.Component {  
+class Schedule extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      isOpen: true
+    }
+  } 
+
+  toggleIsOpen() {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
   render () {
-    return (
-      <View>
-        <Text> My Classes </Text>
-        <SectionList>
-          <eventListObj events={this.props.events} />
-        </SectionList>
-      </View>
-    );
+    if (!isOpen) {
+      console.log('rendering ActionButton');
+      return (
+        <View style={backgroundColor='rgba(255, 255, 255, 0.0)'}>
+          <ActionButton
+            onPress = {this.toggleIsOpen.bind(this)}
+          />
+        </View>
+      )
+
+    } else { //if isOpen
+      console.log('rendering schedule');
+      return (
+        <View style={{flex: 1, backgroundColor:'orange'}}>
+          <Text> My Classes </Text>
+          {console.log(JSON.stringify(eventListObj))}
+          <FlatList>
+            <eventListObj events={this.props.events} />
+          </FlatList>
+          <Button
+            raised
+            accent
+            text="Close"
+            onPress={toggleScheduleOpen.bind(this)}
+          />
+        </View>
+      );
+    }
   }
 }
-
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    // console.log(props.event);
     this.state = {
-      upcoming: {
-        // startTime: new Date(1543489200000),
-        // room: "J152",
-        // name: "CS 4850"
-        startDate: new Date(props.event.startDate),
-        room: props.event.location,
-        name: props.event.title
-      }, 
+      upcoming: null,
       dismissed: false
     };
   }
-
+ 
+  componentDidMount() {
+    if (this.props.events) {
+      //set upcoming event
+      minRemaining = 60; //start with 60 minutes as minimum remaining time because we don't care about anything shorter
+      soonestEvent = null
+      for (event in this.props.events) {
+        remaining = Math.round((new Date(this.props.events[event].startDate).getTime() - Date.now())/1000/60);
+        if (remaining < minRemaining) {
+          minRemaining = remaining;
+          soonestEvent = this.props.events[event]
+        }
+        if (remaining < 60) { //if there is actually something coming up soon
+          this.setState({upcoming: soonestEvent});
+        } else {
+          this.setState({upcoming: null});
+        }
+      }
+    } else {
+      this.setState({upcoming: null});
+    }
+   
+  }
+ 
   toggleDismissed() {
     this.setState({dismissed: !this.state.dismissed})
   }
-
+ 
   render() {
-    if (!this.state.dismissed) {
+    if (!this.state.dismissed && this.state.upcoming) {
       return (
       <View style={styles.header}>
         <Text style={styles.headerText}>
-          {this.state.upcoming.name} in {this.state.upcoming.room} in {Math.round(Math.abs(this.state.upcoming.startDate - new Date())/1000/60)} minutes
+          {this.state.upcoming.title} in {this.state.upcoming.location} in {Math.round((new Date(this.state.upcoming.startDate).getTime() - Date.now())/1000/60)} minutes
         </Text>
         <Button
           raised
+          primary
           text="Dismiss"
           onPress={this.toggleDismissed.bind(this)}
         />
@@ -125,60 +130,103 @@ class Header extends React.Component {
   }
 }
 
+
+
+
 export default class OwlPathNative extends React.Component {
   
-  toggleScheduleOpen() {
-    this.setState({scheduleOpen: !this.state.scheduleOpen});
-  }
-
-  render() {
-    // console.log("events is: "+ JSON.stringify(events) + "\n\n");
-    // console.log("events[0] is: " + JSON.stringify(events[0]));
-    if (this.state.scheduleOpen) {return (
-      <View style={{flex: 1}}>
-        <Header />
-        <Schedule
-          events = {
-            {
-              title: "CS 2408",
-              location: "J201",
-              startDate: new Date()
-            },
-            {
-              title: "CS 4102",
-              location: "J212",
-              startDate: new Date()
-            },
-            {
-              title: "CS 2132",
-              location: "J129",
-              startDate: new Date()
-            }
-          }
-          style={styles.schedule}
-        />
-      </View>
-    );} else { return (
-      <View style={{flex: 1}}>
-        <Header />
-        <WebView>
-          source={{uri: 'http://10.128.54.124:3000/'}}
-          style={{flex: 1}}
-          javaScriptEnabled={true}
-          geolocationEnabled={true}       
-          <ActionButton
-            onPress={this.toggleScheduleOpen}
-          />
-          </WebView>
-      </View>
-    );}
-  }
-
   constructor (props) {
     super(props);
     this.state = {
-      scheduleOpen: false
-    };
+      location: null,
+      events: null
+    }
+  }
+
+  
+
+  async componentDidMount() {
+    loc = await this.getLocationAsync();
+    console.log('got location: ' + JSON.stringify(loc));
+    ev = await this.getEventsAsync();
+    console.log('setting state, got events: ' + JSON.stringify(ev))
+    this.setState({location: [loc.coords.latitude, loc.coords.longitude]});
+    this.setState({events: ev});
+  }
+
+  
+  async getLocationAsync() {
+    const { status } = await Expo.Permissions.askAsync(Expo.Permissions.LOCATION);
+    if (status === 'granted') {
+      console.log('sending location request');
+      return Expo.Location.getCurrentPositionAsync({enableHighAccuracy: true});
+    } else {
+      throw new Error('Location permission not granted');
+    }
+  }
+ 
+  async getEventsAsync() {
+    const { status } = await Expo.Permissions.askAsync(Expo.Permissions.CALENDAR);
+    if (status === 'granted') {
+      var calendars;
+      calendars = await(Expo.Calendar.getCalendarsAsync());
+      console.log('got calendars')
+      var ids = [];
+      for (cal in calendars) {
+        ids.push(calendars[cal].id)
+      }
+      //from one hour ago to one day in the future
+      time = Date.now();
+      console.log('sending event request')
+      return Expo.Calendar.getEventsAsync(ids, time - 3.6e6, time + 6.048e8);
+    } else {
+      throw new Error('Location permission not granted');
+    }
+  }
+
+  render() {
+    if(!(this.state.events && this.state.location)) {
+      // if (!this.state.location) {
+        console.log('rendering load screen');
+        return (
+          <ThemeContext.Provider value={getTheme(uiTheme)}>
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: uiTheme.palette.primaryColor}}>
+              <Text style={{fontSize: 24}}>
+                OwlPath
+              </Text>
+              <Image
+                style={{margin: 20, width: Dimensions.get('window').width * .9}}
+                resizeMode="contain"
+                source={require('./assets/logo.png')}
+              />
+              <Text style={{fontSize: 24}}>
+                Loading . . .
+              </Text>
+            </View>
+          </ThemeContext.Provider>
+        )
+      // if done loading
+      } else {
+        console.log('rendering webview');
+        return (
+          <ThemeContext.Provider value={getTheme(uiTheme)}>
+          <View style={{flex: 1}}>
+            {/* <Header events = {this.props.events.filter(event => new Date(event.startDate).getTime() - Date.now() < 6.048e8)}/> */}
+            <Header events = {this.state.events} />
+            <WebView
+              source={{uri: 'http://10.100.81.150:3000/'}}
+              style={{flex: 1}}
+              javaScriptEnabled={true}
+              geolocationEnabled={true}
+            />
+            <Schedule
+                events = {this.state.events}
+                style={styles.schedule}
+            />
+          </View>
+          </ThemeContext.Provider>
+          );
+        }
   }
 }
 
